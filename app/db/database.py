ad9e460e3a883b99ddd86acc1ee4b0830ba48e20
usman <builtbyusman@gmail.com>
2026-05-03 12:54:03 +0500

    Updated

--- app/db/database.py
+++ app/db/database.py
@@ -3,10 +3,11 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os
 from dotenv import load_dotenv
+from sqlalchemy.pool import NullPool
 
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, poolclass=NullPool)
 session_local = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 
 Base = declarative_base()
